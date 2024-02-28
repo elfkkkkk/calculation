ce7a32eb3c1e601e6fe04e2ec67390462a75441e
elfkkkkk <ekaterinaperminova1460@gmail.com>
2024-02-29 00:11:08 +0700

    555555
    zxc

---
+++ Models/Operation.cs
@@ -0,0 +1,20 @@
+namespace Calculator.Models
+{
+    public enum Operators
+    {
+        MUL,
+        PLUS,
+        MINUS,
+        DIV,
+        MOD,
+        FACTORIAL,
+        POW,
+        LG,
+        LN,
+        SIN,
+        COS,
+        TAN,
+        FLOOR,
+        CEIL
+    }
+}
